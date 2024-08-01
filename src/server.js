ddd3be307c651f31eb156f9ece0cdd25215cac17
Harsh Panchal <harshkumarpanchal04@gmail.com>
2024-08-01 21:07:14 +0530

    modified server adding routes

--- src/server.js
+++ src/server.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const routes = require('./routes/api');
+const {fetchAndStoreData} = require('./services/dataService');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
 app.use(express.json());
+
+app.use('/api',routes);
+
+fetchAndStoreData();
 
 app.listen(PORT,() => {
     console.log(`Server is running on port ${PORT}`);
