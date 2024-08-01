4d43bc41b2d203091f43e734fe57a774a8ca1229
Harsh Panchal <harshkumarpanchal04@gmail.com>
2024-08-01 21:06:26 +0530

    added business logic in routes

---
+++ src/routes/api.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const {readData} = require('../services/dataService');  
+
+const router = express.Router();
+
+router.get('/data', async (req, res) => {
+    try{
+        let data = await readData();
+        const { sortBy , filterBy , filterValue } = req.query;
+
+
+        if(filterBy && filterValue)
+        {
+            data = data.filter(item => item[filterBy].toLowerCase().includes(filterValue.toLowerCase())
+        );
+        }
+
+        if(sortBy)
+        {
+            data = data.sort((a,b) => {
+                if(sortBy === 'version')
+                    {
+                        return a.version = b.version;   
+                    }
+                else if(sortBy === 'name')
+                    {
+                        return a.name.localeCompare(b.name);
+                    }
+                return 0;   
+            }); 
+        }
+        
+        res.json(data);
+    } catch (error) {
+            console.error("Error reading data", error.message);
+            res.status(500).send("Internal Server Error");
+        }
+});
+
+module.exports = router;
