a06328054831eef864086e72c91421bf26cc6de9
Harsh Panchal <harshkumarpanchal04@gmail.com>
2024-08-01 20:52:35 +0530

    Added local fetching and reading service

---
+++ src/services/dataService.js
@@ -0,0 +1,38 @@
+const axios = require('axios');
+const fs = require('fs');
+const path = require('path');
+
+const DATA_URL = 'https://microsoftedge.github.io/Demos/json-dummy-data/256KB.json';
+const DATA_FILE_PATH = path.join(__dirname, "..","data", 'data.json');
+
+
+async function fetchAndStoreData() 
+{
+    try
+    {
+        const response = await axios.getAdapter(DATA_URL);
+        const data = response.data;
+        await fs.writeFile(DATA_FILE_PATH, JSON.stringify(data,null,2));
+        console.log("Data fetched and stored successfully");
+    } catch (error) {
+        console.error("Error fetching data", error.message);
+    }
+}
+
+async function readData()
+{
+    try
+    {
+        const data = await fs.readFile(DATA_FILE_PATH,'utf-8');
+        return JSON.parse(data);
+    } catch (error) {
+        console.error("Error reading data", error.message);
+        return [];
+    }
+}
+
+
+module.exports = {
+    fetchAndStoreData,
+    readData
+}
